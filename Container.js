2cebedc56462b523f7db335acedd9bfb778281e4
Jedeye <jody.septiawan5@gmail.com>
2022-06-30 09:33:59 +0700

    update

--- Container.js
+++ Container.js
@@ -1,10 +1,13 @@
 import * as React from "react";
 
-//Import Navigation Container
+//Import Navigation Container = BrowserRoute
+import { NavigationContainer } from "@react-navigation/native"; 
 
 // Import Stack Navigation
+import { createStackNavigator } from '@react-navigation/stack';
 
 // Import Theme Native Base
+import { useTheme } from "native-base";
 
 // Import Screen
 import FormNativeBase from "./src/screens/formNativeBase";
@@ -12,8 +15,31 @@ import Hello from "./src/screens/hello";
 import IncDec from "./src/screens/incDec";
 
 // Create Stack Navigation
+const Stack = createStackNavigator();
 
 export default function Container() {
   // Init Theme
-  return; // Refactor Code Navigation
+
+  const theme = useTheme()
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" 
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.colors.primary['300']
+        }
+      }}
+      >
+        <Stack.Screen 
+          options={{
+            title: 'Main'
+          }} 
+          name="Home" 
+          component={Hello} 
+        />
+        <Stack.Screen name="IncDec" component={IncDec} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
--- src/screens/hello.js
+++ src/screens/hello.js
@@ -1,15 +1,24 @@
 import * as React from "react";
-import { Text, Box } from "native-base";
+import { Text, Box, Pressable } from "native-base";
 
 // Add Props in Hello({navigation})
-export default function Hello() {
+export default function Hello({ navigation }) {
   return (
     <Box bg="primary.400" flex={1} alignItems="center" justifyContent="center">
       <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
         Life's too short
       </Text>
 
-      {/* Create Button */}
+      <Pressable 
+        onPress={() => navigation.navigate('IncDec')}
+        bg="primary.600" 
+        p={4} 
+        borderRadius={5} 
+        mt={5}>
+        <Text fontFamily="body" fontSize={17}>
+          To Increment Decrement
+        </Text>
+      </Pressable>
     </Box>
   );
 }
